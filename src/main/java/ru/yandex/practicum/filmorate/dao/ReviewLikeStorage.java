6b5b2f1448da9705f81cd89b8f37c0a2f31258b1
stanislavma <msvdev@yandex.com>
2024-05-01 19:58:14 +0500

    style: ReviewLikeStorage.java 'METHOD_DEF' should be separated from previous lin

--- src/main/java/ru/yandex/practicum/filmorate/dao/ReviewLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewLikeStorage.java
@@ -3,9 +3,13 @@ package ru.yandex.practicum.filmorate.dao;
 public interface ReviewLikeStorage {
 
     Long addLike(Long reviewId, Long userId);
+
     void deleteLikes(Long reviewId);
+
     Long addDislike(Long reviewId, Long userId);
+
     void deleteLike(Long reviewId, long userId);
+
     void deleteDislike(Long reviewId, long userId);
 
 }
